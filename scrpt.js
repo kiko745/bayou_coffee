6d395ea31cbe8dad20052f0d606e0d2b6085946d
kiko745 <koussay738@gmail.com>
2025-06-19 17:07:08 +0100

    scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,15 +1,1 @@
-// password.js
 
-const correctPassword = "1010";
-
-// Check if password was already entered this session
-if (sessionStorage.getItem('accessGranted') !== 'true') {
-  const userPassword = prompt("Enter password to access Bayou's Coffee site:");
-
-  if (userPassword === correctPassword) {
-    sessionStorage.setItem('accessGranted', 'true');
-  } else {
-    document.body.innerHTML = "<h1 style='text-align:center; margin-top:20%; font-family:sans-serif;'>Access Denied 🚫</h1>";
-    throw new Error("Wrong password - stopping script execution.");
-  }
-}
